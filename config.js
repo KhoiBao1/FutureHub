dcbe9ef33757e98c3302c09b877e560f392f5600
Bảo Khôi <khoibao928@gmail.com>
2025-01-21 03:35:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
     port: 3000
   },
   database: {
-    connection: 'mongodb+srv://bkhoi:baokhoi352006@cluster0.pptfi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    connection: 'mongodb://bkhoi:baokhoi352006@cluster0.pptfi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
     option: {
       useNewUrlParser: true,
       useUnifiedTopology: true
